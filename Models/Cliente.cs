6f55be14f4c3cdb2c67f5a5d997df89686ffb560
Efraim Fernandes <efraim.fernandes@outlook.com>
2025-02-09 01:15:31 -0300

    Atualizando as namespace e instalando o entity framework

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Barbearia.API.Models.Cliente {
+namespace Barbearia.API.Models {
     public class Cliente {
         [Key]
         public int ClienteId { get; set; }
--- obj/Debug/net8.0/Barbearia.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Barbearia.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barbearia.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f5a6f545aaa3abf33684c3a273435ccbac240d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barbearia.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barbearia.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
