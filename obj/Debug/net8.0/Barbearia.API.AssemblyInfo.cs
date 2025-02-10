edf5faecbe7274180a09923aaf28c56946756a68
Efraim Fernandes <efraim.fernandes@outlook.com>
2025-02-10 17:46:34 -0300

    Adicionando o Application DbContext para o Entity Framework pegar as classes corretas

--- obj/Debug/net8.0/Barbearia.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Barbearia.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barbearia.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f5a6f545aaa3abf33684c3a273435ccbac240d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f55be14f4c3cdb2c67f5a5d997df89686ffb560")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barbearia.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barbearia.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
