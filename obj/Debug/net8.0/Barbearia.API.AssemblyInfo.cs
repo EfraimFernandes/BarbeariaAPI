7be9ab2d567c8ea7d401331ed32b65072010935f
Efraim Fernandes <efraim.fernandes@outlook.com>
2025-02-17 15:38:53 -0300

    Criando um cliente no banco de dados usando o metodo post

--- obj/Debug/net8.0/Barbearia.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Barbearia.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barbearia.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc366f03befe8355014dca094aab4938ec2b009")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae6d18c70c141384f786b3aa6f7ce9ea07695a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barbearia.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barbearia.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
